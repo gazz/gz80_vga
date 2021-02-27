27f2d3ba572bdb69f57e3d134d0d1a382f921c32
Janis Dancis <gazz@digihaze.com>
2021-02-26 22:46:55 -0700

    use bios include for referenced function calls

--- vga.asm
+++ vga.asm
@@ -1,33 +1,4 @@
-ClearDisplay equ 0xf5
-PIOOut equ 0x5C2
-ApplicationReturn equ 0x39B
-; set caret anywhere on the screen
-; b - offset, 64 (0x40) is 2nd line
-; c - offset on the line
-SetCaretRaw_bios equ 0x11A
-
-OutHex equ $04EE
-NumToHex equ $0293
-DisableCursor equ $050A
-Wait_PS2ScanCode equ $056B
-StringOut equ 0xc2
-
-; there are RAM mapped
-STATE_KEYSTROKE equ $203E
-STATE_SCANCODE equ $2042
-Wait equ 0x60
-
-; PIO direct access
-PORTA 	equ 0x4
-PORTB 	equ 0x5
-PORTAC 	equ 0x6
-PORTBC 	equ 0x7
-
-PORT2A 	equ 0xc
-PORT2B 	equ 0xd
-PORT2AC equ 0xe
-PORT2BC equ 0xf
-
+#include "../gz80_bios/bios_include.asm" 
 
 #target bin
 #code _CODE, 0x2082, 0x100
@@ -39,18 +10,20 @@ PORT2BC equ 0xf
 TXT .asciz "VGA TEST!"
 
 main:
-	call ClearDisplay
+	call cleardisplay
 
 runloop:
+	ld b, 0
+	ld c, 0
+	call set_carret_raw
 
 	ld hl, TXT
-	call StringOut
+	call mstringout
 
-	; wait for input
-	call Wait_PS2ScanCode
+	call longwait
 
 	jr runloop
 
 exit:
 	;halt
-	jp ApplicationReturn
+	jp app_ret
